f8ccfb4d634ec670ca41e16310a5f3cb2a263f75
Gustavo Ramirez <tavroot.work@gmail.com>
2024-04-15 15:50:55 +0200

    Hardcoded some things to get pure GMRES (only) working

--- src/init.c
+++ src/init.c
@@ -169,8 +169,13 @@ void method_setup( vector_double *V, level_struct *l, struct Thread *threading )
 #ifdef INIT_ONE_PREC
     } else {
 #endif
+      //printf0("we're here!\n");
+      //exit(0);
+      //fgmres_double_struct_alloc( g.restart, g.max_restart, l->inner_vector_size, g.tol,
+      //                            _GLOBAL_FGMRES, _RIGHT, preconditioner,
+      //                            g.method==6?g5D_plus_clover_double:d_plus_clover_double, &(g.p), l );
       fgmres_double_struct_alloc( g.restart, g.max_restart, l->inner_vector_size, g.tol,
-                                  _GLOBAL_FGMRES, _RIGHT, preconditioner,
+                                  _GLOBAL_FGMRES, _NOTHING, NULL,
                                   g.method==6?g5D_plus_clover_double:d_plus_clover_double, &(g.p), l );
     }
 #ifdef INIT_ONE_PREC
